cf8ae814ae487731a408aa96f5acb27248ae45dd
4727yesuraju <4727yesuraju@gmail.com>
2023-09-30 16:05:00 +0530

    ll

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     useEffect(() => {
         fetchApiConfig();
         genresCall();
-    }, []);
+    });
 
     const fetchApiConfig = () => {
         fetchDataFromApi("/configuration").then((res) => {
